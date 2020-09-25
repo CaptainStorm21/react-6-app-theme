36b531bf3e8b4533d2ad550794a345cec79eb367
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-09-24 19:08:22 -0700

    popup and theme

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,18 +1,50 @@
 import React, { Component } from "react";
-// styles
-import "./styles.css";
+
 
 class Modal extends Component {
+  closeCustomModal = (e) => {
+    this.props.onCloseModal(e);
+  };
   render() {
-    if (!this.props.showModal) {
-      return null;
-    }
     return (
-      <div className="modal">
-        <div className="modal_content">
-          {this.props.children}
-          <div>
-            <button onClick={this.props.toggleModal}>Close</button>
+      <div id="myModal" >
+        <div className="modal-content">
+          <div className="modal-header">
+            <span className="close" onClick={this.closeCustomModal}>
+              &times;
+            </span>
+            <h4>Modal Header</h4>
+          </div>
+
+          <div className="container-fluid">
+            <p>
+              Ea et ad cillum incididunt ex occaecat consectetur fugiat cillum.
+              Magna enim voluptate minim id ullamco nisi veniam pariatur tempor
+              ea exercitation ea. Elit irure magna ipsum ad aute esse mollit ea
+              officia incididunt nulla velit ullamco. Exercitation laborum id
+              enim Lorem laborum esse adipisicing nisi ipsum nulla anim est
+              cupidatat id. Incididunt nulla do adipisicing reprehenderit.
+            </p>
+            <p>
+              Ea et ad cillum incididunt ex occaecat consectetur fugiat cillum.
+              Magna enim voluptate minim id ullamco nisi veniam pariatur tempor
+              ea exercitation ea. Elit irure magna ipsum ad aute esse mollit ea
+              officia incididunt nulla velit ullamco. Exercitation laborum id
+              enim Lorem laborum esse adipisicing nisi ipsum nulla anim est
+              cupidatat id. Incididunt nulla do adipisicing reprehenderit.
+            </p>
+            <p>
+              Ea et ad cillum incididunt ex occaecat consectetur fugiat cillum.
+              Magna enim voluptate minim id ullamco nisi veniam pariatur tempor
+              ea exercitation ea. Elit irure magna ipsum ad aute esse mollit ea
+              officia incididunt nulla velit ullamco. Exercitation laborum id
+              enim Lorem laborum esse adipisicing nisi ipsum nulla anim est
+              cupidatat id. Incididunt nulla do adipisicing reprehenderit.
+            </p>
+          </div>
+
+          <div className="modal-footer">
+            <h3>Modal Footer</h3>
           </div>
         </div>
       </div>
@@ -20,4 +52,6 @@ class Modal extends Component {
   }
 }
 
-export { Modal };
+export default Modal;
+
+
